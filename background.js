b8abc3156336ff9c91579c9e83d45c76e0b1f34c
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2015-05-12 12:32:22 -0300

    Enable user to toggle subtitles

--- background.js
+++ background.js
@@ -2,3 +2,12 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {file: "content_script.js"});
 });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      // read `newIconPath` from request and read `tab.id` from sender
+      chrome.browserAction.setIcon({
+        path: request.newIconPath,
+        tabId: sender.tab.id
+      });
+});
--- content_script.js
+++ content_script.js
@@ -1,5 +1,31 @@
 // Remove subtitles
-var subtitles = document.getElementsByClassName('player-timedtext')
-for(index in subtitles) {
-  subtitles[index].remove();
+
+var css   = '.player-timedtext { opacity: 0!important; }',
+    head  = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+style.type = 'text/css';
+style.setAttribute('id', 'hide-subtitle')
+
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}
+
+toggle = localStorage.toggleSubtitles
+elem   = document.getElementById('hide-subtitle');
+
+// Normalize toggle
+if(toggle && !elem)
+  toggle = null;
+
+if (toggle) {
+  elem.remove();
+  localStorage.removeItem('toggleSubtitles');
+  chrome.runtime.sendMessage({ "newIconPath" : "19.png" });
+} else {
+  head.appendChild(style);
+  localStorage.toggleSubtitles = true;
+  chrome.runtime.sendMessage({ "newIconPath" : "19-undo.png" });
 }
