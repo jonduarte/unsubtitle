cc6e7227753ac053f6b0699ccb824e9425547b30
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2022-07-26 11:29:27 +0200

    Update extension to manifest v3

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
+
 // When clicked call javascript inside the content of a tab
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content_script.js']
+  });
 });
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      // read `newIconPath` from request and read `tab.id` from sender
-      chrome.browserAction.setIcon({
-        path: request.newIconPath,
-        tabId: sender.tab.id
-      });
-});
+  function (request, sender, sendResponse) {
+    // read `newIconPath` from request and read `tab.id` from sender
+    chrome.action.setIcon({
+      path: request.newIconPath,
+      tabId: sender.tab.id
+    });
+  }
+);
--- content_script.js
+++ content_script.js
@@ -1,31 +1,31 @@
 // Remove subtitles
 
-var css   = '.player-timedtext { opacity: 0!important; }',
-    head  = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
+var css = '.player-timedtext { opacity: 0!important; }',
+  head = document.head || document.getElementsByTagName('head')[0],
+  style = document.createElement('style');
 
 style.type = 'text/css';
 style.setAttribute('id', 'hide-subtitle')
 
-if (style.styleSheet){
+if (style.styleSheet) {
   style.styleSheet.cssText = css;
 } else {
   style.appendChild(document.createTextNode(css));
 }
 
 toggle = localStorage.toggleSubtitles
-elem   = document.getElementById('hide-subtitle');
+elem = document.getElementById('hide-subtitle');
 
 // Normalize toggle
-if(toggle && !elem)
+if (toggle && !elem)
   toggle = null;
 
 if (toggle) {
   elem.remove();
   localStorage.removeItem('toggleSubtitles');
-  chrome.runtime.sendMessage({ "newIconPath" : "19.png" });
+  chrome.runtime.sendMessage({ "newIconPath": "19.png" });
 } else {
   head.appendChild(style);
   localStorage.toggleSubtitles = true;
-  chrome.runtime.sendMessage({ "newIconPath" : "19-undo.png" });
+  chrome.runtime.sendMessage({ "newIconPath": "19-undo.png" });
 }
